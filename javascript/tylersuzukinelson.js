5365590dd33620a9cd153cc2ba95083f1f4c1692
Tyler Suzuki Nelson <tyler.nelson@alumni.ubc.ca>
2015-02-25 16:54:18 -0800

    Added UBC to education list

--- javascript/tylersuzukinelson.js
+++ javascript/tylersuzukinelson.js
@@ -1,8 +1,25 @@
 $(document).ready(function() {
+
   $('.project').on('mouseenter', function() {
     $(this).find('.project-description').fadeIn();
   });
+
   $('.project').on('mouseleave', function() {
     $(this).find('.project-description').fadeOut();
   });
+
+  $('.school').on('click', '.showClasses', function() {
+    $(this).removeClass('showClasses').show();
+    $(this).addClass('hideClasses').show();
+    $(this).text('Hide Classes');
+    $(this).siblings('.classes').show();
+  });
+
+  $('.school').on('click', '.hideClasses', function() {
+    $(this).removeClass('hideClasses').show();
+    $(this).addClass('showClasses').show();
+    $(this).text('Show Classes');
+    $(this).siblings('.classes').hide();
+  });
+
 });
